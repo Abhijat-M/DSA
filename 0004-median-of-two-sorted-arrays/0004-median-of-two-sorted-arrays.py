f7bf5217f08ddab95978413b1bcb5312a3fa25aa
Abhijat <abhijatknight003@gmail.com>
2025-04-12 16:13:51 +0530

    Time: 0 ms (100%), Space: 18 MB (59.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,14 +1,36 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
-        numbers = sorted(nums1 + nums2)
-        n = len(numbers)
-    
-        if n % 2 == 1:
-            median = numbers[n // 2]
-        else:
-            mid_index1 = n // 2 - 1
-            mid_index2 = n // 2
-            median = (numbers[mid_index1] + numbers[mid_index2]) / 2
 
-        return float(median)
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        x, y = len(nums1), len(nums2)
+        low, high = 0, x
+
+        while low <= high:
+            
+            partitionX = (low + high) // 2
+            partitionY = (x + y + 1) // 2 - partitionX
+            
+            #num1
+            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            minRightX = float('inf') if partitionX == x else nums1[partitionX]
+
+            #num2
+            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minRightY = float('inf') if partitionY == y else nums2[partitionY]
+
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+
+                if (x + y) % 2 == 0:
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
+                else:
+                    return float(max(maxLeftX, maxLeftY))
+
+            elif maxLeftX > minRightY:
+                high = partitionX - 1
+
+            else:
+                low = partitionX + 1
+
+        raise ValueError("Input arrays are not valid")
