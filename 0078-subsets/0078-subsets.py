80f0a5d496755f5358b5431171587a5e26c36039
Abhijat <abhijatknight003@gmail.com>
2025-05-25 08:43:19 +0530

    Time: 0 ms (100%), Space: 18.1 MB (21.45%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,9 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        out=[]
-        out.append([])
-        if len(nums)==1:
-            out.append(nums)
-            return out
-        for i in range(1,len(nums)):
-            for j in combinations(nums,i):
-                out.append(list(j))
-        out.append(nums)
-        return out
+class Solution: 
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+
+        result: list[list[int]] = [[]]
+
+        for num in nums:
+            result += [curr + [num] for curr in result]
+
+        return result
