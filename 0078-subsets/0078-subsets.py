7374f4c2b4afd2d1af9b73d9fd4f81abf79a4288
Abhijat <abhijatknight003@gmail.com>
2025-05-25 08:53:56 +0530

    Time: 0 ms (100%), Space: 17.9 MB (40.25%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,9 +1,23 @@
 class Solution: 
     def subsets(self, nums: list[int]) -> list[list[int]]:
 
-        result: list[list[int]] = [[]]
+        # result: list[list[int]] = [[]]
 
-        for num in nums:
-            result += [curr + [num] for curr in result]
+        # for num in nums:
+        #     result += [curr + [num] for curr in result]
 
+        # return result
+
+        result: list = []
+
+        def back(nums, curr):
+            if len(nums) == 0:
+                result.append(curr)
+                return
+            back(nums[1:], curr + [nums[0]])
+            back(nums[1:], curr)
+
+            return
+
+        back(nums, [])
         return result
