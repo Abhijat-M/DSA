63cee90beb32b91862dd7df57b6eb867c763318d
Abhijat <abhijatknight003@gmail.com>
2025-05-26 09:19:02 +0530

    Time: 0 ms (100%), Space: 17.8 MB (67.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,30 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        res = []
         if not digits:
-            return []
+            return res
 
-        digit_to_letters_map = [
-            "0", "0", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-        ]
-        result = self._solve1_recursive(digits, digit_to_letters_map)
-        return result
+        phone_map = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
 
-    def _solve1_recursive(self, digits: str, mapping: List[str]) -> List[str]:
-        if not digits:
-            return [""]
+        def helper(pro, unpro):
+            if not unpro:
+                res.append(pro)
+                return
+            for char in phone_map[unpro[0]]:
+                helper(pro + char, unpro[1:])
 
-        first_digit_char = digits[0]
-        digit_value = int(first_digit_char)
-        letters_for_first_digit = mapping[digit_value]
-        
-        remaining_digits = digits[1:]
-        combinations_from_rest = self._solve1_recursive(remaining_digits, mapping)
-        current_result = []
-
-        for letter in letters_for_first_digit:
-            for combo in combinations_from_rest:
-                current_result.append(letter + combo)
-        
-        return current_result
-
-           
+        helper("", digits)
+        return res
+            
